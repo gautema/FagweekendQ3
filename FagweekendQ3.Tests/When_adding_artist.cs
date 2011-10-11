5004aa0b61be9ae4bf4c2e47add70266fde9c73d
Gaute Magnussen <gaute.magnussen@gmail.com>
2011-10-11 21:57:16 +0200

    Fixed some merge conflicts

---
+++ FagweekendQ3.Tests/When_adding_artist.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FagweekendQ3.Controllers;
+using FagweekendQ3.DataStore;
+using FagweekendQ3.ViewModels;
+using NUnit.Framework;
+
+namespace FagweekendQ3.Tests
+{
+    [TestFixture]
+    public class When_adding_artist
+    {
+        private ArtistController _controller;
+        private IArtistStore _artistStore;
+
+        [SetUp]
+        public void Setup()
+        {
+            _artistStore = new ArtistStore();
+            _controller = new ArtistController(_artistStore);
+
+            _controller.Edit(new ArtistViewModel {Id = Guid.Empty.ToString(), Name = "Iron Maiden"});
+        }
+
+        [Test]
+        public void Should_add_artist_to_data_store()
+        {
+            Assert.That(_artistStore.GetAll().Count(), Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Should_save_artist_name()
+        {
+            Assert.That(_artistStore.GetAll().Any(x => x.Name == "Iron Maiden"));
+
+        }
+    }
+}
