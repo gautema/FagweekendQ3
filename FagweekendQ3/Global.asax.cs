81b541c75ea354a204819edaff49136cf7f2ac2c
Gaute Magnussen <gaute.magnussen@gmail.com>
2011-10-11 21:24:58 +0200

    Added action filter for logging to Debug console

--- FagweekendQ3/Global.asax.cs
+++ FagweekendQ3/Global.asax.cs
@@ -4,17 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using FagweekendQ3.Tools;
 
 namespace FagweekendQ3
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
 
     public class MvcApplication : System.Web.HttpApplication
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new LoggingFilter());
         }
 
         public static void RegisterRoutes(RouteCollection routes)
---
+++ FagweekendQ3/Tools/LoggingFilter.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace FagweekendQ3.Tools
+{
+    public class LoggingFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Debug.WriteLine(string.Format("Loaded {0} in {1}", filterContext.ActionDescriptor.ActionName, filterContext.ActionDescriptor.ControllerDescriptor.ControllerName));
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Debug.WriteLine(string.Format("Exited {0} in {1}", filterContext.ActionDescriptor.ActionName, filterContext.ActionDescriptor.ControllerDescriptor.ControllerName));
+        }
+    }
+}
